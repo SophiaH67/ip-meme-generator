57e328a904445daefab85a6c75f266fec0abb1ee
marnixah <marnix@hagehoeve.com>
2022-02-10 00:24:26 +0100

    feat(main): dynamically generate & send videos

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"log"
 	"memegenerator/meme"
+	"net/http"
 )
 
-func main() {
-	ip := "127.0.0.1"
+func handler(w http.ResponseWriter, r *http.Request) {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+	log.Printf("%s requested %s", ip, r.URL.Path)
+
 	m := meme.New(ip)
 	m.Render()
+
+	http.ServeFile(w, r, m.OutputPath)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
